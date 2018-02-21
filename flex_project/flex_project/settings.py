56283cfce34fcb00fe59e7dc1eba8e012cddd2a8
Rewel Garcia <rewelgarcia@Rewels-Air.T-mobile.com>
2018-02-20 22:52:45 -0800

    Update settings file to find the templates directory

--- flex_project/flex_project/settings.py
+++ flex_project/flex_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'flex_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'build'),
+)
