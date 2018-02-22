56b34087fbe402d9180d4ecf05be485d76cef2c8
rewel garcia <rgjr22@gmail.com>
2018-02-22 08:56:07 -0800

    Create User view

--- flex_project/write_away/views.py
+++ flex_project/write_away/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect, Http404, HttpResponseForbidden
 
 class IndexView(TemplateView):
     template_name = "application.html"
+
+class User():
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
