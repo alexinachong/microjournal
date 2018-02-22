b60dd036215e081e1a4cbe9f099e982b53335c2b
rewel garcia <rgjr22@gmail.com>
2018-02-21 18:31:24 -0800

    Fix settings file

--- flex_project/flex_project/settings.py
+++ flex_project/flex_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'write-away',
+    'write_away',
     'rest_framework',
     'jquery'
 ]
