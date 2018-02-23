fd9d820156412440021f1eba3a781f92690490ee
Alexina Chong <alexina.chong@gmail.com>
2018-02-22 19:07:05 -0800

    Add button icons to feed

--- flex_project/flex_project/settings.py
+++ flex_project/flex_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo',
+    'write_away',
     'rest_framework',
     'jquery'
 ]
